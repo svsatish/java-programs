851d3e2c3e31ef0e3187e792283a38e1d5230a6e
satish <satishatgmu@gmail.com>
2025-03-02 21:26:41 -0500

    Sort & Merge Arrays

---
+++ src/test/java/sdet/SortArray.java
@@ -0,0 +1,15 @@
+package sdet;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+    public static void main(String[] args){
+
+        int[] arr = {5, 2, 8, 1, 3};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
