45a9b4a20f8c8e778a7cca59f95dd9c9edc61fb0
satish <satishatgmu@gmail.com>
2025-03-04 18:43:04 -0500

    Remove whitespace

---
+++ src/test/java/sdet/RemoveWhiteSpacesinString.java
@@ -0,0 +1,11 @@
+package sdet;
+
+public class RemoveWhiteSpacesinString {
+
+    public static void main(String[] args){
+
+        String str = "I am in  terv iewing now";
+
+        System.out.println(str.replace(" ", ""));
+    }
+}
