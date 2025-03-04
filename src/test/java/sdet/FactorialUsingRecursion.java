93e56c6b993986484cbdfdd94d3ad2d880151337
satish <satishatgmu@gmail.com>
2025-03-04 16:48:33 -0500

    Factorial using recursion

---
+++ src/test/java/sdet/FactorialUsingRecursion.java
@@ -0,0 +1,15 @@
+package sdet;
+
+public class FactorialUsingRecursion {
+
+    public static void main(String[] args){
+
+        System.out.println(fact(5));
+    }
+
+    static int fact(int n){
+        if (n==0) return 1;
+
+        return n * fact(n-1);
+    }
+}
