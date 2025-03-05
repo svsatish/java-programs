c8e056a7191b39a7ee270d024a09b721cc60aa81
satish <satishatgmu@gmail.com>
2025-03-05 11:15:34 -0500

    LengthOfString

---
+++ src/test/java/sdet/LengthOfString.java
@@ -0,0 +1,11 @@
+package sdet;
+
+public class LengthOfString {
+
+    public static void main(String[] args) {
+
+        String str = "Automation";
+
+        System.out.println(str.length());
+    }
+}
