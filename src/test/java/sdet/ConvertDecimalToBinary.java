40c9d5f4c033aefdf361309cf1301a0a6d18b8fa
satish <satishatgmu@gmail.com>
2025-03-05 15:55:08 -0500

    ConvertDecimalToBinary

---
+++ src/test/java/sdet/ConvertDecimalToBinary.java
@@ -0,0 +1,12 @@
+package sdet;
+
+public class ConvertDecimalToBinary {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        System.out.println(Integer.toBinaryString(num));
+
+    }
+}
